5c7f64e410afdb582b21fd2605ee90e8df1e84d3
chenquan <chenquan@shiyuegame.com>
2017-04-27 21:43:33 +0800

    新增资源

--- Assets/ChangeSkin/Editor/PrefabSync/SyncPrefabWindow.cs
+++ Assets/ChangeSkin/Editor/PrefabSync/SyncPrefabWindow.cs
@@ -49,16 +49,16 @@ namespace PrefabSync
                 }
             }
 
-            if(GUILayout.Button("替换图片", GUILayout.Width(100)))
-            {
-                string path = "Assets/Textures/UI/MainUI_/I18NUpgradeButtonIcon.png";
-                string createPath = "Assets/UI/Image/final/I18NUpgradeButtonIcon.png";
-                bool result = FileUtil.DeleteFileOrDirectory(path);
-                FileUtil.CopyFileOrDirectory(createPath, path);
-                GameObject.Find("Canvas/Old/Test/UpgradeButton").GetComponent<Image>().sprite =
-                    AssetDatabase.LoadAssetAtPath(path, typeof(Sprite)) as Sprite;
-                AssetDatabase.Refresh();
-            }
+            //if(GUILayout.Button("替换图片", GUILayout.Width(100)))
+            //{
+            //    string path = "Assets/Textures/UI/MainUI_/I18NUpgradeButtonIcon.png";
+            //    string createPath = "Assets/UI/Image/final/I18NUpgradeButtonIcon.png";
+            //    bool result = FileUtil.DeleteFileOrDirectory(path);
+            //    FileUtil.CopyFileOrDirectory(createPath, path);
+            //    GameObject.Find("Canvas/Old/Test/UpgradeButton").GetComponent<Image>().sprite =
+            //        AssetDatabase.LoadAssetAtPath(path, typeof(Sprite)) as Sprite;
+            //    AssetDatabase.Refresh();
+            //}
         }
     }
 }
